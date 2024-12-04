1bef4f1b41a1de67fd92fb0f63cfca7138e58a47
wolfman0719 <hsato0719@icloud.com>
2024-12-04 16:57:46 +0900

    Update tatekae.zsh

--- tatekae.zsh
+++ tatekae.zsh
@@ -1,5 +1,7 @@
 # 立て替え費用計算
 # tatekae.zsh 年 月
+# 例 2024年12月分を作成する 
+# irispython tatekae.zsh 2024 12
 echo "エクセルファイルコピー" 
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/*.xlsx /Users/hsatoctr/work/expense
 echo "立て替え費用計算" 
