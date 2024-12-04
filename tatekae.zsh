8337aeedb477c9a230477fc96c0f992875afe0ec
wolfman0719 <hsato0719@icloud.com>
2024-12-04 16:54:03 +0900

    Add files via upload

---
+++ tatekae.zsh
@@ -0,0 +1,10 @@
+# 立て替え費用計算
+# tatekae.zsh 年 月
+echo "エクセルファイルコピー" 
+cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/*.xlsx /Users/hsatoctr/work/expense
+echo "立て替え費用計算" 
+irispython tatekae.py .
+echo "立て替え費用をを元のディレクトリにコピーする"
+cp /Users/hsatoctr/work/expense/立て替え.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
+echo "エクセルファイルを削除"
+# rm /Users/hsatoctr/work/expense/*.xlsx
