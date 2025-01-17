0144b95a7af90b2c1d4864899632d912353f430e
wolfman0719 <hsato0719@icloud.com>
2025-01-17 14:02:01 +0900

    Add files via upload

---
+++ travelreport.zsh
@@ -0,0 +1,12 @@
+# 出張報告精算書作成
+# ワークディレクトリにコピー
+cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/work/expense/
+# travelreport.zsh 年 月 日
+# 例 1202出張報告精算書.xlsxを編集する 
+# irispython travelreport.zsh 2024 12 02
+echo "出張報告精算書作成 $2$3出張報告精算書.xlsx" 
+irispython createtravelreport.py /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx
+echo "出張報告精算書を元のディレクトリにコピーする"
+cp /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
+echo "エクセルファイルを削除"
+rm /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx
