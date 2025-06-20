9a5417f26b88c5ca64950cbe107458b0fa56bfbc
wolfman0719 <hsato0719@icloud.com>
2025-06-20 09:30:34 +0900

    Add files via upload

--- expense.zsh
+++ expense.zsh
@@ -1,14 +1,12 @@
-# 出張報告精算書作成
+# 経費ファイルを日付でソート
 # expense.zsh 年 月 日
 # 例 1202出張報告精算書.xlsxを編集する 
-# irispython expense.zsh 2024 12 02
+# sudo python3 expense.zsh 2024 12
 echo "エクセルファイルコピー" 
-cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/work/expense
+cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense
 echo "経費ファイル日付でソート中 $1$2経費.xlsx" 
 irispython sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
-echo "出張報告精算書作成 $2$3出張報告精算書.xlsx" 
-irispython createtravelreport.py /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx
-echo "出張報告精算書を元のディレクトリにコピーする"
-cp /Users/hsatoctr/work/expense/*.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
+echo "経費ファイルを元のディレクトリにコピーする"
+cp /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
 echo "エクセルファイルを削除"
-# rm /Users/hsatoctr/work/expense/*.xlsx
+rm /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
--- tatekae.zsh
+++ tatekae.zsh
@@ -1,12 +1,11 @@
 # 立て替え費用計算
 # tatekae.zsh 年 月
-# 例 2024年12月分を作成する 
-# irispython tatekae.zsh 2024 12
+echo 
 echo "エクセルファイルコピー" 
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/*.xlsx /Users/hsatoctr/work/expense
 echo "立て替え費用計算" 
-irispython tatekae.py .
+python3 tatekae.py .
 echo "立て替え費用をを元のディレクトリにコピーする"
 cp /Users/hsatoctr/work/expense/立て替え.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
 echo "エクセルファイルを削除"
-# rm /Users/hsatoctr/work/expense/*.xlsx
+rm /Users/hsatoctr/work/expense/*.xlsx
