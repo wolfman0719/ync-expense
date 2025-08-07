dd2ec30da01f7d0c722dc5fcaa73792b97c501a2
wolfman0719 <hsato0719@icloud.com>
2025-08-07 12:12:00 +0900

    Update expense.zsh

--- expense.zsh
+++ expense.zsh
@@ -5,7 +5,8 @@
 echo "エクセルファイルコピー" 
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense
 echo "経費ファイル日付でソート中 $1$2経費.xlsx" 
-irispython sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
+#irispython sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
+python3 sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
 echo "経費ファイルを元のディレクトリにコピーする"
 cp /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
 echo "エクセルファイルを削除"
