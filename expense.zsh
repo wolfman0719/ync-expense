25b9d749c3fab537367e595bfd88c33a9fb6c405
wolfman0719 <hsato0719@icloud.com>
2025-10-10 12:01:38 +0900

    Add files via upload

--- expense.zsh
+++ expense.zsh
@@ -2,10 +2,16 @@
 # expense.zsh 年 月 日
 # 例 1202出張報告精算書.xlsxを編集する 
 # sudo python3 expense.zsh 2024 12
+export IRISINSTALLDIR=/opt/iris
+export LD_LIBRARY_PATH=$IRISINSTALLDIR/bin:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$IRISINSTALLDIR/bin:$DYLD_LIBRARY_PATH
+export IRISUSERNAME=_system
+export IRISPASSWORD=SYS
+export IRISNAMESPACE=USER
 echo "エクセルファイルコピー" 
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense
 echo "経費ファイル日付でソート中 $1$2経費.xlsx" 
-#irispython sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
+# irispython sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
 python3 sortbymd.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx
 echo "経費ファイルを元のディレクトリにコピーする"
 cp /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
--- travelreport2.zsh
+++ travelreport2.zsh
@@ -1,12 +1,19 @@
 # 出張報告精算書作成
 # ワークディレクトリにコピー
+export IRISINSTALLDIR=/opt/iris
+export LD_LIBRARY_PATH=$IRISINSTALLDIR/bin:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$IRISINSTALLDIR/bin:$DYLD_LIBRARY_PATH
+export IRISUSERNAME=_system
+export IRISPASSWORD=SYS
+export IRISNAMESPACE=USER
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense
 cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/work/expense/
 # travelreport2.zsh 年 月 日
 # 例 1202出張報告精算書.xlsxを編集する 
 # python3 travelreport2.zsh 2024 12 02
 echo "出張報告精算書作成 $2$3出張報告精算書.xlsx"
-irispython createtravelreport2.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx "$2" "$3"
+# irispython createtravelreport2.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx "$2" "$3"
+python3 createtravelreport2.py /Users/hsatoctr/work/expense/"$1""$2"経費.xlsx /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx "$2" "$3"
 echo "出張報告精算書を元のディレクトリにコピーする"
 cp /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
 echo "エクセルファイルを削除"
