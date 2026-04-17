8b3388ec9c4cfd8c5f2b4b806890286e640facae
wolfman0719 <hsato0719@icloud.com>
2026-04-17 13:48:24 +0900

    Update travelreport.zsh

--- travelreport.zsh
+++ travelreport.zsh
@@ -5,7 +5,7 @@ cp /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/"$2""$3"出張報告精算書.xlsx /
 # 例 1202出張報告精算書.xlsxを編集する 
 # python3 travelreport.zsh 2024 12 02
 echo "出張報告精算書作成 $2$3出張報告精算書.xlsx" 
-python3 createtravelreport4.py /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx
+python3 createtravelreport5.py /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx
 echo "出張報告精算書を元のディレクトリにコピーする"
 cp /Users/hsatoctr/work/expense/"$2""$3"出張報告精算書.xlsx /Users/hsatoctr/Dropbox/yncorp/"$1"/"$2"/
 echo "エクセルファイルを削除"
